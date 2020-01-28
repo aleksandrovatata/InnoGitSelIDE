f25749227c9c69b8280e98917a4a1d6d530cfe34
Olena.Teslenko <alenateslenko13@gmail.com>
2020-01-28 17:41:40 +0200

    first commit

---
+++ src/test/java/AnotherTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+
+public class AnotherTest extends WebDriverSettings{
+
+
+    @Test
+    public void supportButton(){
+        driver.findElement(By.className("zh-support")).click();
+    }
+}
